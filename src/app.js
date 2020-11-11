109a04259e4c92bdd46c7c827e636f4c41faeb51
unknown <ashirwadsharma12795@gmail.com>
2020-11-11 18:04:53 +0530

    Dynamic page routing using hbs. use also for static page

--- src/app.js
+++ src/app.js
@@ -5,21 +5,30 @@ console.log(__dirname)
 
 const app = express()
 const publicDirectoryPath = path.join(__dirname,'../public')
+const viewPath = path.join(__dirname,'../views')
 
-
+app.set('view engine', 'hbs')
+app.set('views',viewPath)
 app.use(express.static(publicDirectoryPath))
+
+
 
 // app.get('',(req,res)=>{
 //     res.send('<h1>Hello express</h1>')
 // })
+app.get('',(req,res)=>{
+    res.render('index',{title:'Weather',name:'Ashirwad'})
+})
 
-// app.get('/help',(req,res)=>{
-//     res.send([{ name : 'Ashirwad'}, { name:'Sharma' }])
-// })
 
-// app.get('/about',(req,res)=>{
-//     res.send('<h1>About page</h1>')
-// })
+app.get('/help',(req,res)=>{
+    res.send('help',{ helptxt: 'This is help txt'}])
+})
+
+app.get('/about',(req,res)=>{
+    res.send('about',{ title: 'About me', name:'Ashirwad Sharma' }])
+
+})
 
 app.get('/weather',(req,res)=>{
     res.send({
