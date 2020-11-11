a51eb815f56c42156f3c8ce8b4a7d59634812fc0
unknown <ashirwadsharma12795@gmail.com>
2020-11-11 22:43:04 +0530

    Showing weather result of searching location

--- public/js/app.js
+++ public/js/app.js
@@ -1,12 +1,30 @@
 console.log("client side java script")
 
-fetch('http://localhost:3000/weather?location=Varanasi').then((responce)=>{
+
+const weatherForm = document.querySelector('form')
+const search = document.querySelector('input')
+const msg1 = document.querySelector('#message-1')
+const msg2 = document.querySelector('#message-2')
+
+weatherForm.addEventListener('submit',(e)=>{
+    e.preventDefault()
+
+    const location = search.value
+    msg1.textContent = 'Loading...'
+    msg2.textContent = ''
+    // console.log(location)
+    fetch('http://localhost:3000/weather?location='+location).then((responce)=>{
     responce.json().then((data)=>{
         if(data.error){
-            console.log(data.error)
+            // console.log(data.error)
+            msg1.textContent = data.error;
         }else{
-            console.log(data.location);
-            console.log(data.forcast);
+            // console.log(data.location);
+            // console.log(data.forcast);
+            msg1.textContent = data.location
+            msg2.textContent = data.forcast
         }
     })
+})
+
 })
