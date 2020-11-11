d32527c71bc669ed28ad467293862f75e3636c74
unknown <ashirwadsharma12795@gmail.com>
2020-11-11 21:58:37 +0530

    Fetching data from api at client side

--- public/js/app.js
+++ public/js/app.js
@@ -1,1 +1,12 @@
 console.log("client side java script")
+
+fetch('http://localhost:3000/weather?location=Varanasi').then((responce)=>{
+    responce.json().then((data)=>{
+        if(data.error){
+            console.log(data.error)
+        }else{
+            console.log(data.location);
+            console.log(data.forcast);
+        }
+    })
+})
